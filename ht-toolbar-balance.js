44f25f7f6837544958b07acca829769150999f0f
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2018-03-04 20:38:15 +0300

    created

---
+++ ht-toolbar-balance.js
@@ -0,0 +1,44 @@
+"use strict";
+import { LitElement, html } from "../@polymer/lit-element/lit-element.js";
+import "../@polymer/paper-styles/default-theme.js";
+
+class HTToolbarBalance extends LitElement {
+  render({ href, balance }) {
+    return html`
+      <style>
+        :host {
+          display: block;
+          position: relative;
+          box-sizing: border-box;
+        }
+      
+        a {
+          display:flex;
+          align-items:center;
+            text-decoration: none;
+            color: inherit;
+            outline: none;
+            color:var(--secondary-text-color);
+        }
+
+        #balance {
+          font-size:16px;
+        }
+      </style>
+
+      <a href$=${href}>
+          <div id="balance">$${balance + ".00"}</div>
+      </a>
+`;
+  }
+
+  static get is() {
+    return "ht-toolbar-balance";
+  }
+
+  static get properties() {
+    return { href: String, balance: Number };
+  }
+}
+
+customElements.define(HTToolbarBalance.is, HTToolbarBalance);
